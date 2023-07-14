35bdcfee34a70ae2dfcbd4de2567ba72c8fc200e
Brittany Brimley <brittanybrimley@brittanys-mbp.lan>
2023-07-14 06:33:57 -0600

    Edited text in HTML and added/began code/pseudocode for script.js.

---
+++ assets/js/script.js
@@ -0,0 +1,53 @@
+// put variables here
+
+// create function for countdown timer
+function countDown() {
+    // create var and if statements for what happens when you complete quiz before timer
+    // create else if/else statments for what happens if you do not complete quiz within 75seconds
+        // create message that appears if time runs out "Time's Up!"
+};
+
+// create function to subtract time when answer selected is wrong
+function subractTime() {
+
+};
+
+function generateScore() {
+    var quizQuestions = ("Commonly used data types DO NOT include: ");
+    // Answers: 1. Strings, 2. Booleans, 3. Alerts, 4. Numbers
+    // Correct response: 3. Alerts
+    subractTime();
+    if (confirm("The condition in an if/else statement is enclosed within ___."));
+        // Answers: 1. Quotes, 2. Curly Brackets, 3. Parentheses, 4. Square Brackets
+        // Correct response: 3. Parentheses
+        subractTime();
+    if (confirm("Arrays in JavaScript can be used to store ___."));
+        // Answers: 1. Numbers and Strings, 2. Other Arrays, 3. Booleans, 4. All of the above
+        // Correct response: 4. All of the above
+        subractTime();
+    if (confirm("String values must be enclosed within ___ when being assigned to variables."))
+        // Answers: 1. Commas, 2. Curly Brackets, 3. Quotes, 4. Parentheses
+        // Correct response: 3. Quotes
+        subractTime();
+    if (confirm("A very useful tool used during development and debugging for printing content to the debugger is:"))
+        // Answers: 1. JavaScript, 2. Terminal/Bash, 3. for loops, 4. console.log
+        // Correct response: 4. console.log
+        subractTime();
+    } else {
+        // else statment to caluclate  question responses for score
+        for ()
+    };
+    };
+    return builder;
+};
+
+// addEventListener for start quiz button
+startButton.addEventListener("click", function(event) {
+    countDown();
+});
+
+// addEventListener and/or function for selecting and logging answers?
+
+// addEventListener and/or function for generating score?
+
+// addEventListener and/or function for saving initials and score.
