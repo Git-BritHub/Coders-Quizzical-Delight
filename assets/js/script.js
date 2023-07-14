0230baf8ddcd0f878ccb11887a4b434d90f1329e
Brittany Brimley <brittanybrimley@brittanys-mbp.lan>
2023-07-14 13:19:38 -0600

    setting up layout of webpage

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,33 +12,37 @@ function subractTime() {
 
 };
 
+var quizQuestions = [
+    {
+        question: "Commonly used data types DO NOT include: ",
+        choices: ["1. Strings", "2. Booleans", "3. Alerts", "4. Numbers"],
+        correct: "3. Alerts"
+    },
+    {
+        question: "The condition in an if/else statement is enclosed within ___.",
+        choices: ["1. Quotes", "2. Curly Brackets", "3. Parentheses", "4. Square Brackets"],
+        correct: "3. Parentheses"
+    },
+    {
+        question: "Arrays in JavaScript can be used to store ___.",
+        choices: ["1. Numbers and Strings", "2. Other Arrays", "3. Booleans", "4. All of the above"],
+        correct: "4. All of the above"
+    },
+    {
+        question: "String values must be enclosed within ___ when being assigned to variables.",
+        choices: ["1. Commas", "2. Curly Brackets", "3. Quotes", "4. Parentheses"],
+        correct: "3. Quotes"
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        choices: ["1. JavaScript", "2. Terminal/Bash", "3. for loops", "4. console.log"],
+        correct: "3. console.log"
+    },
+]
+
 function generateScore() {
-    var quizQuestions = ("Commonly used data types DO NOT include: ");
-    // Answers: 1. Strings, 2. Booleans, 3. Alerts, 4. Numbers
-    // Correct response: 3. Alerts
-    subractTime();
-    if (confirm("The condition in an if/else statement is enclosed within ___."));
-        // Answers: 1. Quotes, 2. Curly Brackets, 3. Parentheses, 4. Square Brackets
-        // Correct response: 3. Parentheses
-        subractTime();
-    if (confirm("Arrays in JavaScript can be used to store ___."));
-        // Answers: 1. Numbers and Strings, 2. Other Arrays, 3. Booleans, 4. All of the above
-        // Correct response: 4. All of the above
-        subractTime();
-    if (confirm("String values must be enclosed within ___ when being assigned to variables."))
-        // Answers: 1. Commas, 2. Curly Brackets, 3. Quotes, 4. Parentheses
-        // Correct response: 3. Quotes
-        subractTime();
-    if (confirm("A very useful tool used during development and debugging for printing content to the debugger is:"))
-        // Answers: 1. JavaScript, 2. Terminal/Bash, 3. for loops, 4. console.log
-        // Correct response: 4. console.log
-        subractTime();
-    } else {
-        // else statment to caluclate  question responses for score
-        for ()
-    };
-    };
-    return builder;
+
+   
 };
 
 // addEventListener for start quiz button
